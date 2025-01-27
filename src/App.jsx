c603e92d55dc2bce8c9361409e6c412c9c9e9485
Reginaldo Campelo <regin.developer@gmail.com>
2025-01-27 10:29:03 -0300

    adding tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-const App = () => {
+function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
